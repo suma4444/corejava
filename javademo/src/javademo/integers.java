e17118a9bcb4cee2a6e51d800262bcd7abf0b081
akula <akula@SUMA>
2022-06-25 18:37:25 +0530

    integers

---
+++ javademo/src/javademo/integers.java
@@ -0,0 +1,17 @@
+package javademo;
+
+public class integers {
+
+	public static void main(String[] args) 
+	{
+		 int max=1234;
+		 int min=-1235;
+		 short shortmax=3276;
+		 short shortmin=-3278;
+		 long longmax=678997654L;
+		 long longmin=-678997655L;
+		 System.out.println("int"+min);
+
+	}
+
+}
