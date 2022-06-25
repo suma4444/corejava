91c2212bbf3433a3359ad903b58b5ada75864ad3
akula <akula@SUMA>
2022-06-25 21:41:48 +0530

    Boolean

---
+++ javademo/src/javademo/Boolean.java
@@ -0,0 +1,15 @@
+package javademo;
+
+public class Boolean {
+
+	public static void main(String[] args) 
+	{
+		boolean var=false;//true or false (0 or 1)
+		System.out.println(var);
+		char var1='\u00A7';//$,*,.,
+		System.out.println(var1);
+		
+
+	}
+
+}
