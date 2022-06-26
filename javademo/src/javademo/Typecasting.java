3c978d0009a01bde65a24be8eebeb72c5a0a5488
akula <akula@SUMA>
2022-06-26 10:43:19 +0530

    typecasting

---
+++ javademo/src/javademo/Typecasting.java
@@ -0,0 +1,14 @@
+package javademo;
+
+public class Typecasting {
+
+	public static void main(String[] args) 
+	{
+		 int num = 10;
+		    System.out.println("The integer value: " + num);
+		    double data = num;
+		    System.out.println("The double value: " + data);
+
+	}
+
+}
