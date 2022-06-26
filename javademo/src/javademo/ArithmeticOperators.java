f2359166b27efd5409730bbabfe641860059c8ff
akula <akula@SUMA>
2022-06-26 11:24:19 +0530

    ArithmeticOperators

---
+++ javademo/src/javademo/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package javademo;
+
+public class ArithmeticOperators 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 5, y=60;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+}
+
+}
