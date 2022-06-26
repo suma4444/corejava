6bc2e836b1c2596f30d7e2e97262e115f7ef97a1
akula <akula@SUMA>
2022-06-26 14:48:45 +0530

    nestedif statement

---
+++ javademo/src/javademo/NestedIf.java
@@ -0,0 +1,19 @@
+package javademo;
+
+public class NestedIf 
+{
+
+	public static void main(String[] args) 
+	{
+		   int age=20;  
+		    int weight=80;    
+		    if(age>=18)
+		    {    
+		        if(weight>50)
+		        {  
+		            System.out.println("You are eligible to donate blood");  
+		        }    
+		    }    
+	}
+
+}
