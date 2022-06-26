6f70167569a48315cdb1bc8c843795b4edc673ad
akula <akula@SUMA>
2022-06-26 12:01:32 +0530

    Bignumber

---
+++ javademo/src/javademo/Bignumber.java
@@ -0,0 +1,18 @@
+package javademo;
+
+public class Bignumber {
+
+	public static void main(String[] args) 
+	{
+	int A=8,B=9;
+	if(A>B)
+	{
+		System.out.println("true");
+	}
+	else
+	{
+		System.out.println("false");
+	}
+	
+}
+	}
