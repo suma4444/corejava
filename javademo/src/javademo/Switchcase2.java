16bbe4362b311f01f243be8135b34c6b51e02f6a
akula <akula@SUMA>
2022-06-26 20:18:19 +0530

    Switchcase(char)

---
+++ javademo/src/javademo/Switchcase2.java
@@ -0,0 +1,26 @@
+package javademo;
+
+public class Switchcase2 
+{
+
+	public static void main(String[] args) 
+	{
+	    char alphabet = 'b';  
+        switch(alphabet) 
+        {  
+              
+              case 'a': 
+              System.out.println("This is character 'a' ");
+              break;
+              case 'b':
+              System.out.println("This is character 'b' ");
+              break;
+              case 'c':
+              System.out.println("This is character 'c' ");
+              break;  
+               
+              default: System.out.println("Please enter valid input");  
+         }  
+	}
+
+}
