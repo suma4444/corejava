6f91edfab0636a0b452001c83efc06a5dac5fed1
akula <akula@SUMA>
2022-06-26 11:55:41 +0530

    ExactNumber

---
+++ javademo/src/javademo/ExactNumber.java
@@ -0,0 +1,19 @@
+  package javademo;
+
+public class ExactNumber {
+
+	public static void main(String[] args) 
+	{
+		int a=5;
+		if(a!=5)
+		{
+			System.out.println("value of x is not 5 ");
+		
+		}
+		else
+		{
+			System.out.println("value of x is 5");
+		}
+	}
+
+}
