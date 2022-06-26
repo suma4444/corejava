408dea18fc98fc93da278506cae3523e1fa9bcac
akula <akula@SUMA>
2022-06-26 10:35:56 +0530

    String

---
+++ javademo/src/javademo/Hellos.java
@@ -0,0 +1,16 @@
+package javademo;
+
+public class Hellos {
+
+	public static void main(String[] args) 
+	{
+		 String first = "Java";
+		    String second = "Python";
+		    String third = "JavaScript";
+
+		  
+		    System.out.println(first); 
+		    System.out.println(second);  
+		    System.out.println(third);   
+		  }
+}
