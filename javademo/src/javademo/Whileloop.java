1387dac4cee94c24dc62465160a189ad5b9cbe7d
akula <akula@SUMA>
2022-06-26 15:13:20 +0530

    whileloop

---
+++ javademo/src/javademo/Whileloop.java
@@ -0,0 +1,16 @@
+package javademo;
+
+public class Whileloop 
+{
+public static void main(String[] args)
+  {
+	   int i = 1;
+	   while(i < 6) 
+       {
+           System.out.println("Hello World");
+           i++;
+       }
+
+	}
+
+}
