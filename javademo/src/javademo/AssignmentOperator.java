905406072dbff96f3d410ccbdd5f775ea6419840
akula <akula@SUMA>
2022-06-26 14:35:35 +0530

    AssignmentOperator

---
+++ javademo/src/javademo/AssignmentOperator.java
@@ -0,0 +1,18 @@
+package javademo;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) 
+	{
+		  int a = 4;
+		    int var;
+		    var = a;
+		    System.out.println("var using =: " + var);
+		    var += a;
+		    System.out.println("var using +=: " + var);
+		    var *= a;
+		    System.out.println("var using *=: " + var);	
+
+	}
+
+}
