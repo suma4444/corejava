308ab99f2c56850a6f8eb547cc1315aa2dc6a71d
akula <akula@SUMA>
2022-06-27 13:51:16 +0530

    switchcase(string)

---
+++ javademo/src/javademo/Switchcase3.java
@@ -0,0 +1,24 @@
+package javademo;
+
+public class Switchcase3 
+{
+public static void main(String[] args) 
+	{
+	 String str = "two";
+switch (str) 
+     {
+    case "one":
+        System.out.println("one");
+         break;
+    case "two":
+    	System.out.println("two");
+        break;
+    case "three":
+        System.out.println("three");
+        break;
+    default:
+        System.out.println("no match");
+    }	
+	}
+
+}
